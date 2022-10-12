2e306ee375133cc8897ce4ab683b27b49547e800
Edwin Brown <teddybrown@ischool.berkeley.edu>
2022-10-11 20:21:40 -0700

    clean up create_db.py

--- create_db.py
+++ create_db.py
@@ -1,16 +1,13 @@
-"import os
 import io
 import sqlite3
-import pandas as pd
-import numpy as np
-from PIL import Image
-from pathlib import Path
 from hashlib import blake2b
+from pathlib import Path
 
-ROOT_DIR = Path(
-    r"C:\Users\teddy\Documents\01-Berkeley\W207\facial-keypoints-detection"
-)
-TEST_DATA = ROOT_DIR.joinpath("data","test.csv")
+import pandas as pd
+from PIL import Image
+
+ROOT_DIR = Path(r"C:\Users\teddy\Documents\01-Berkeley\W207\facial-keypoints-detection")
+TEST_DATA = ROOT_DIR.joinpath("data", "test.csv")
 TRAIN_DATA = ROOT_DIR.joinpath("data", "training.csv")
 TRAIN_DB = ROOT_DIR.joinpath("db", "training.db")
 
@@ -35,18 +32,18 @@ def display_image(png_string):
     return
 
 
-def calc_hash(input):
+def calc_hash(input_bytes):
     """
     Calculate the blake2b hash for the png image
     """
-    myhash = blake2b()
-    myhash.update(input)
-    return myhash.hexdigest()
+    my_hash = blake2b()
+    my_hash.update(input_bytes)
+    return my_hash.hexdigest()
 
 
 def main():
     """
-    Generate an sqlite database from a pandas table.
+    Generate sqlite database from a pandas table.
     :return:
     :rtype:
     """
@@ -60,43 +57,6 @@ def main():
     sqcur.execute("""PRAGMA temp_store=2""")
     sqcur.execute("""PRAGMA locking_mode=EXCLUSIVE""")
     sqcur.execute("""PRAGMA cache_size=-65536""")
-    # sqcur.execute("""PRAGMA synchronous=OFF""")
-    # sqcur.execute("""PRAGMA journal_mode=MEMORY""")
-    # sqcur.execute(
-    #     """CREATE TABLE IF NOT EXISTS training_data(
-    #                     image_id INTEGER PRIMARY KEY,
-    #                     left_eye_center_x REAL,
-    #                     left_eye_center_y REAL,
-    #                     right_eye_center_x REAL,
-    #                     right_eye_center_y REAL,
-    #                     left_eye_inner_corner_x REAL,
-    #                     left_eye_inner_corner_y REAL,
-    #                     left_eye_outer_corner_x REAL,
-    #                     left_eye_outer_corner_y REAL,
-    #                     right_eye_inner_corner_x REAL,
-    #                     right_eye_inner_corner_y REAL,
-    #                     right_eye_outer_corner_x REAL,
-    #                     right_eye_outer_corner_y REAL,
-    #                     left_eyebrow_inner_end_x REAL,
-    #                     left_eyebrow_inner_end_y REAL,
-    #                     left_eyebrow_outer_end_x REAL,
-    #                     left_eyebrow_outer_end_y REAL,
-    #                     right_eyebrow_inner_end_x REAL,
-    #                     right_eyebrow_inner_end_y REAL,
-    #                     right_eyebrow_outer_end_x REAL,
-    #                     right_eyebrow_outer_end_y REAL,
-    #                     nose_tip_x REAL,
-    #                     nose_tip_y REAL,
-    #                     mouth_left_corner_x REAL,
-    #                     mouth_left_corner_y REAL,
-    #                     mouth_right_corner_x REAL,
-    #                     mouth_right_corner_y REAL,
-    #                     mouth_center_top_lip_x REAL,
-    #                     mouth_center_top_lip_y REAL,
-    #                     mouth_center_bottom_lip_x REAL,
-    #                     mouth_center_bottom_lip_y REAL,
-    #                     image BLOB)"""
-    # )
     df.to_sql("training_data", sqcon, if_exists="replace", index=True)
     sqcon.commit()
     sqcon.close()
