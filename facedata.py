30074f31b289446086e3a7f91c6e39dc4cc957e7
Edwin Brown <teddybrown@ischool.berkeley.edu>
2022-10-13 06:26:15 -0700

    group data into faces and points for viewing; nothing with machine learning yet

---
+++ facedata.py
@@ -0,0 +1,30 @@
+from dataclasses import dataclass
+
+
+@dataclass
+class Point:
+    x: int
+    y: int
+
+
+@dataclass
+class FaceData:
+    """FaceData for facial-keypoints-detection"""
+
+    rowid: int
+    png_hash: str
+    left_eye_center: Point
+    right_eye_center: Point
+    left_eye_inner_corner: Point
+    left_eye_outer_corner: Point
+    right_eye_inner_corner: Point
+    right_eye_outer_corner: Point
+    left_eyebrow_inner_end: Point
+    left_eyebrow_outer_end: Point
+    right_eyebrow_inner_end: Point
+    right_eyebrow_outer_end: Point
+    nose_tip: Point
+    mouth_left_corner: Point
+    mouth_right_corner: Point
+    mouth_center_top_lip: Point
+    mouth_center_bottom_lip: Point
--- image_utils.py
+++ image_utils.py
@@ -4,6 +4,8 @@ from io import BytesIO
 from pathlib import Path
 
 from PIL import Image, ImageDraw
+from facedata import FaceData
+from facedata import Point
 
 
 def get_image_from_db(sqcur):
@@ -20,7 +22,7 @@ def get_image_from_db(sqcur):
     return image
 
 
-def get_points_from_db(sqcur):
+def get_face_points_from_db(sqcur, rowid, png_hash):
     """
 
     :param sqcur:
@@ -29,13 +31,75 @@ def get_points_from_db(sqcur):
     :rtype:
     """
     sqcur.execute(
-        """SELECT rowid, png_hash, left_eye_center_x, left_eye_center_y FROM image_data LIMIT 1"""
+        """SELECT   rowid, 
+                    png_hash,   
+                    left_eye_center_x, 
+                    left_eye_center_y, 
+                    right_eye_center_x,
+                    right_eye_center_y,
+                    left_eye_inner_corner_x,
+                    left_eye_inner_corner_y,
+                    left_eye_outer_corner_x,
+                    left_eye_outer_corner_y,
+                    right_eye_inner_corner_x,
+                    right_eye_inner_corner_y,
+                    right_eye_outer_corner_x,
+                    right_eye_outer_corner_y,
+                    left_eyebrow_inner_end_x,
+                    left_eyebrow_inner_end_y,
+                    left_eyebrow_outer_end_x,
+                    left_eyebrow_outer_end_y,
+                    right_eyebrow_inner_end_x,
+                    right_eyebrow_inner_end_y,
+                    right_eyebrow_outer_end_x,
+                    right_eyebrow_outer_end_y,
+                    nose_tip_x,
+                    nose_tip_y,
+                    mouth_left_corner_x,
+                    mouth_left_corner_y,
+                    mouth_right_corner_x,
+                    mouth_right_corner_y,
+                    mouth_center_top_lip_x,
+                    mouth_center_top_lip_y,
+                    mouth_center_bottom_lip_x,
+                    mouth_center_bottom_lip_y FROM image_data WHERE rowid = ? AND png_hash = ?""",
+        (
+            rowid,
+            png_hash,
+        ),
     )
+    rows = sqcur.fetchall()
+    if len(rows) == 0:
+        print("Didn't find any rows. Poorly Specified query?")
+        return None
+    if len(rows) > 1:
+        print("This should Never Happen. More than one row")
+        return None
+    result = FaceData(
+        rows[0][0],
+        rows[0][1],
+        Point(rows[0][2], rows[0][3]),
+        Point(rows[0][4], rows[0][5]),
+        Point(rows[0][6], rows[0][7]),
+        Point(rows[0][8], rows[0][9]),
+        Point(rows[0][10], rows[0][11]),
+        Point(rows[0][12], rows[0][13]),
+        Point(rows[0][14], rows[0][15]),
+        Point(rows[0][16], rows[0][17]),
+        Point(rows[0][18], rows[0][19]),
+        Point(rows[0][20], rows[0][21]),
+        Point(rows[0][22], rows[0][23]),
+        Point(rows[0][24], rows[0][25]),
+        Point(rows[0][26], rows[0][27]),
+        Point(rows[0][28], rows[0][29]),
+        Point(rows[0][30], rows[0][31]),
+    )
+    return FaceData
 
 
-##
-## Some proof of concept image stuff. To how it works.
-##
+#
+# Some proof of concept image stuff. To how it works.
+#
 def main():
     """
     Generate sqlite database from a pandas table.
@@ -65,6 +129,9 @@ def main():
     sqcon = sqlite3.connect(db)
     sqcur = sqcon.cursor()
 
+    row_id = 1
+    my_png_hash = "2dd2b74d17693c1290fde72ede8c08edcfbcc86818933ab65a4d6e87ae83bd1db305cbff4d65690dbdf1753d905aee7e68cbe7b2c2f4d8d879aa2c655f2c4521"
+    face_points = get_face_points_from_db(sqcur, row_id, my_png_hash)
     image_data = get_image_from_db(sqcur)
     buf_image_data = BytesIO(image_data)
     im = Image.open(buf_image_data).convert("RGB")
